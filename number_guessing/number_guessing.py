d26e790403f2d4653f8be4d80d1d34dd9d14a9ee
Jake Urrunaga <jakeurrunaga@endpoint-10-192-191-173.ufvisitor.ufl.edu>
2026-02-27 14:47:37 -0500

    Completed a short project learning to use Numpy and Matplotlib; Scrapped some older files.

--- number_guessing/number_guessing.py
+++
@@ -1,6 +0,0 @@
-#importing the random module (default python) --- could be useful for protein data! if its a database like thing lol
-import random
-
-#last integer will not be considered, first will; (-1, 11) is really -1 - 10
-r = random.randrange(-1, 11)
-print(r)
